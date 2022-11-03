1a92b971a1ca6d1b7e0bf4cdae139549050053ad
aknibir-cse <aknibir.cse@gmail.com>
2022-11-04 02:15:56 +0600

    Content Updated

---
+++ src/com/selenium/browser/GoogleChromeOpen.java
@@ -0,0 +1,28 @@
+package com.selenium.browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleChromeOpen {
+
+	public static void main(String[] args) {
+		
+		//Path of Executable Driver Path
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Eclipse-Chrome-Driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		//Parent p = new Child (WebDriver is Parent & ChromeDriver is Child) 
+
+		driver.get("https://www.google.com/");
+		//Opening URL
+		
+		System.out.println(driver.getTitle());
+		//Printing Title
+		
+		//driver.quit();
+		//Closing Browser
+	}
+
+}
